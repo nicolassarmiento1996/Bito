06cabb7650853d982ccc47289b7615cb8967489a
nicolassarmiento1996 <nicolassarmientogarcia@gmail.com>
2025-02-03 22:06:59 -0500

    Update app.py

--- app.py
+++ app.py
@@ -11,25 +11,31 @@ if "logged_in" not in st.session_state:
     st.session_state.logged_in = False
     st.session_state.username = ""
 
+# Fondo con la imagen y el color de texto blanco
 page_bg_img = """
 <style>
 [data-testid="stAppViewContainer"] {
-background-image: url("https://img.freepik.com/foto-gratis/personas-que-toman-clases-pilates-reformador_23-2151093272.jpg?t=st=1738638246~exp=1738641846~hmac=c98b8738e3217cfda46863036a62ca7f8745ab9d61d03d3e99de187a0da9ea6a&w=2000");
-background-size: cover;
+    background-image: url("https://img.freepik.com/foto-gratis/personas-que-toman-clases-pilates-reformador_23-2151093272.jpg?t=st=1738638246~exp=1738641846~hmac=c98b8738e3217cfda46863036a62ca7f8745ab9d61d03d3e99de187a0da9ea6a&w=2000");
+    background-size: cover;
+    color: white;  /* Establecer color blanco para el texto */
 }
 
 [data-testid="stHeader"] {
-background-color: rgba(0, 0, 0, 0);
+    background-color: rgba(0, 0, 0, 0);
 }
 
 [data-testid="stToolbar"] {
-right: 2rem;
+    right: 2rem;
 }
+
+h1, h2, h3, h4, h5, h6, p {
+    color: white; /* Asegura que todos los textos sean blancos */
+}
+
 </style>
 """
 
 st.markdown(page_bg_img, unsafe_allow_html=True)
-st.title("It's summer!")
 
 def login():
     """Función para manejar el inicio de sesión"""
